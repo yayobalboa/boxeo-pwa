32d82151bcc36f4040a1f081d9ab2bf3f927b1d6
yayobalboa <yairbalboa@hotmail.com>
2026-04-28 12:50:48 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,9 +12,19 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
+  // Ignorar las peticiones a la API de Google Apps Script
+  if (event.request.url.includes("script.google.com")) {
+      return; 
+  }
+  
   if (event.request.method !== 'GET') return;
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
